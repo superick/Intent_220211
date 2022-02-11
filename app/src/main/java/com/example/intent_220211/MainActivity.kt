776ee159115685a0fc9616184c5a629d936da4f6
superick <superick@naver.com>
2022-02-11 21:23:00 +0900

    Intent 이용 다른화면으로 이동

--- app/src/main/java/com/example/intent_220211/MainActivity.kt
+++ app/src/main/java/com/example/intent_220211/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intent_220211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+//            다른 화면으로 이동
+
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
     }
 }
