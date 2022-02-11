5aef92a77ca58b03e6a0cfc854f99a2a51154771
superick <superick@naver.com>
2022-02-11 21:11:53 +0900

    다른 화면 추가 UI작업 추가

---
+++ app/src/main/java/com/example/intent_220211/OtherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_220211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
