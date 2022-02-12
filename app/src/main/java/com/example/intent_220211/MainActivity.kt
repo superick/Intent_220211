60cac4f4b754a1e2384546f4f7a51637183942a7
superick <superick@naver.com>
2022-02-12 16:10:08 +0900

    입력한 문구를 데이터 송부

--- app/src/main/java/com/example/intent_220211/MainActivity.kt
+++ app/src/main/java/com/example/intent_220211/MainActivity.kt
@@ -10,6 +10,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sendMessageBtn.setOnClickListener {
+
+//            입력한 내용을 변수에 저장
+
+            val inputMessage = messgeEdt.text.toString()
+
+//            비행기 티켓 발권
+            val myIntent = Intent(this, messageAct::class.java)
+
+//            수하물 첨부
+
+            myIntent.putExtra("message", inputMessage)
+
+//            실제 출발
+
+            startActivity(myIntent)
+
+
+       }
+
         moveToOtherBtn.setOnClickListener {
 //            다른 화면으로 이동
 
