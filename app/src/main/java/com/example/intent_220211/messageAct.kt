a26bc72eb8221ee66f0a62dae27010bb4c42c45f
superick <superick@naver.com>
2022-02-12 16:20:15 +0900

    전달해준 문구를 화면에서 보여줌

--- app/src/main/java/com/example/intent_220211/messageAct.kt
+++ app/src/main/java/com/example/intent_220211/messageAct.kt
@@ -2,10 +2,16 @@ package com.example.intent_220211
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class messageAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+        val intentMessage = intent.getStringExtra("message")
+
+        recevedMessageTxt.text = intentMessage
+
     }
 }
