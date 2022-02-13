2c477409bebb095e2653c5d89f245e21dff14965
superick <superick@naver.com>
2022-02-13 09:23:50 +0900

    닉네임 변경화면에서 입력후 완료 클릭시 데이터 가지고 돌아가기

--- app/src/main/java/com/example/intent_220211/EditNickName.kt
+++ app/src/main/java/com/example/intent_220211/EditNickName.kt
@@ -1,5 +1,6 @@
 package com.example.intent_220211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nick_name.*
@@ -8,6 +9,18 @@ class EditNickName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+
+            val inputNewNickname = newNickNameEdt.text.toString()
+
+            val resultInetent = Intent()
+            resultInetent.putExtra("nickname", inputNewNickname)
+            setResult(RESULT_OK, resultInetent)
+            finish()
+
+
+        }
 
 
 
